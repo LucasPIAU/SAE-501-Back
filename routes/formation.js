0ac07a84fcf32cd06b366ac0be9b8dcf48546b10
LPI <Lucas.Piau.Etu@univ-lemans.fr>
2025-01-27 14:38:11 +0100

    Version prod

--- routes/formation.js
+++ routes/formation.js
@@ -9,123 +9,17 @@ import deleteFormationsController from '../controllers/formations/deleteFormatio
 const formationRoutes = express();
 
 // Routes GET
+formationRoutes.get('/all', getFormationsController.getAllFormations);
 
-formationRoutes.get('/all', async (req, res) => {
-    try {
-        const db = await connectToDB();
+formationRoutes.get('/pro', getFormationsController.getProFormations);
 
-        const collectionFormationsPro = db.collection('formationsPro');
-        const collectionFormationsTechno = db.collection('formationsTechno');
-        const collectionOptionsSeconde = db.collection('optionSeconde');
-        const collectionOptionsGenerale = db.collection('formationsGenerale');
+formationRoutes.get('/techno', getFormationsController.getTechnoFormations);
 
-        const formationsPro = await collectionFormationsPro.find().toArray();
-        const formationsTechno = await collectionFormationsTechno.find().toArray();
-        const optionsSeconde = await collectionOptionsSeconde.find().toArray();
-        const optionsGenerale = await collectionOptionsGenerale.find().toArray();
+formationRoutes.get('/opt/seconde', getFormationsController.getOptSeconde);
 
-        if (
-            formationsPro.length > 0 ||
-            formationsTechno.length > 0 ||
-            optionsSeconde.length > 0 ||
-            optionsGenerale.length > 0
-        ) {
-            res.status(200).json({
-                formationsPro,
-                formationsTechno,
-                optionsSeconde,
-                optionsGenerale
-            });
-        } else {
-            res.status(404).json({ message: "Aucune formation trouvée dans les collections" });
-        }
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue lors de la récupération des formations : ${err.message}` });
-    }
-});
+formationRoutes.get('/opt/generale', getFormationsController.getOptGenerale);
 
-formationRoutes.get('/pro', async (req, res) => {
-    try {
-        const db = await connectToDB();
-        const collectionFormations = db.collection('formationsPro');
-
-        const formations = await collectionFormations.find().toArray();
-
-        if (formations.length > 0) {
-            res.status(200).json(formations);
-        } else res.status(404).json({ message: "Aucune formations trouvée" });
-
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération des formations : ${err}` });
-    }
-});
-
-formationRoutes.get('/techno', async (req, res) => {
-    try {
-        const db = await connectToDB();
-        const collectionFormations = db.collection('formationsTechno');
-
-        const formations = await collectionFormations.find().toArray();
-
-        if (formations.length > 0) {
-            res.status(200).json(formations);
-        } else res.status(404).json({ message: "Aucune formations trouvée" });
-
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération des formations : ${err}` });
-    }
-});
-
-formationRoutes.get('/opt/seconde', async (req, res) => {
-    try {
-        const db = await connectToDB();
-        const collectionFormations = db.collection('option-seconde');
-
-        const formations = await collectionFormations.find().toArray();
-
-        if (formations.length > 0) {
-            res.status(200).json(formations);
-        } else res.status(404).json({ message: "Aucune formations trouvée" });
-
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération des formations : ${err}` });
-    }
-});
-
-formationRoutes.get('/opt/generale', async (req, res) => {
-    try {
-        const db = await connectToDB();
-        const collectionFormations = db.collection('option-generale');
-
-        const formations = await collectionFormations.find().toArray();
-
-        if (formations.length > 0) {
-            res.status(200).json(formations);
-        } else res.status(404).json({ message: "Aucune formations trouvée" });
-    }  catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération d'une formation : ${err}` });
-    }
-    })
-
-formationRoutes.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        // On se connect à la base de donnée et on récupère la collection formations
-        const db = await connectToDB();
-        const collectionFormations = db.collection('Formations');
-
-        // On fait la requêtes pour récupérer la formation qui correspond à l'id passer en paramètre
-        if (id) {
-            const query = { '_id': new ObjectId(id) };
-            console.log(query);
-            const formation = await collectionFormations.findOne(query);
-            if (formation) res.status(200).json(formation);
-            else res.status(404).json({ message: "Aucune formation trouvé pour cet id" });
-        } else throw "L'id de la formations est obligatoire";
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération d'une formation : ${err}` });
-    }
-});
+formationRoutes.get('/:id', getFormationsController.getFormationById);
 
 // Routes POST
 
--- routes/lycee.js
+++ routes/lycee.js
@@ -1,51 +1,21 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
-import connectToDB from '../functions/connectDb.js';
+import lyceeValidator from '../middleware/lyceeValidator.js';
+import getEtablissementController from '../controllers/etablissements/getEtablissementController.js';
+import patchEtablissementController from '../controllers/etablissements/patchEtablissementController.js';
+import deleteEtablissementController from '../controllers/etablissements/deleteEtablissementController.js';
+import postEtablissementController from '../controllers/etablissements/postEtablissementController.js';
 
 const lyceeRoutes = express();
 
 // Routes GET
-lyceeRoutes.get('/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        // Récupération d'un lycée
-        res.status(200).json('OK');
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur interne est survenue dans la récupération d'un lycée : ${err}` });
-    }
-});
 
-lyceeRoutes.get('', async (req, res) => {
-    try {
-        // On se connect à la base de donnée et on récupère la collection établissements
-        const db = await connectToDB();
-        const collectionEtablissements = db.collection('etablissements');
+lyceeRoutes.get('/', getEtablissementController.getLycee);
+lyceeRoutes.get('/:id', getEtablissementController.getLyceeById);
 
-        // On fait la requête pour récupérer la liste de toute les établissements
-        const cursor = collectionEtablissements.find();
-        const etablissements = await cursor.toArray();
+// Routes Patch
 
-        // Une fois qu'on as tout récupérer on renvoie les données
-        if(etablissements.length > 0) res.status(200).json(etablissements);
-        else res.status(404).json({message: "Aucun établissement trouvé"});
-
-        // Récupération de la liste de tout les lycées
-        // res.status(200).json('OK');
-    } catch (err) {
-        res.status(500).json({message: `Une erreur interne est survenue dans la récupération des lycées : ${err}`});
-    }
-});
-
-// Routes PUT
-lyceeRoutes.put('/:id', [auth], async (req, res) => {
-    const { id } = req.params;
-    try {
-        // Modification d'un lycée avec les nouvelles données
-        res.status(200).json('OK');
-    } catch (err) {
-        res.status(500).json({ message: `Une erreur est survenue pendant la modification d'un lycée : ${err}` });
-    }
-});
+lyceeRoutes.patch('/edit/:id', [auth, lyceeValidator], patchEtablissementController.patchLycee);
 
 // Routes POST
 
